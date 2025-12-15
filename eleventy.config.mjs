94efc0cf53604d64e6365cc1fbc626da2d51db23
Whyme Lyu <callme5long@gmail.com>
2025-12-16 07:13:44 +0800

    Basically works.

---
+++ eleventy.config.mjs
@@ -0,0 +1,28 @@
+import pluginWebc from "@11ty/eleventy-plugin-webc"
+import { feedPlugin } from "@11ty/eleventy-plugin-rss"
+
+export default function(c) {
+  const site = {
+    name: "Random Bugs",
+    desc: "A blog by @5long",
+    base: "https://5long.github.io",
+  }
+
+  c.addPlugin(pluginWebc)
+  c.addGlobalData("RB.siteName", site.name)
+  c.addGlobalData("RB.desc", site.desc)
+
+  c.addPlugin(feedPlugin, {
+    type: "atom",
+    outputPath: "atom.xml",
+    collection: {
+      name: "post",
+      limit: 0,
+    },
+    base: site.base,
+    author: {
+      name: "Whyme Lyu",
+      email: "callme5long@gmail.com",
+    },
+  })
+}
---
+++ post/fresh-start.md
@@ -0,0 +1,15 @@
+---
+tags: post
+layout: post
+date: 2025-12-17
+title: A Fresh Start Always Feels Good
+---
+
+I've decided to abandom my old blog and start fresh for the Nth time
+that I've lost count. It always feels good to just do so. It might
+be a loss to throw away all the old stuff but I don't care that much
+about it anyway.
+
+And I need an excuse to start writing.
+
+Some of the old posts might be preserved if I feel like it.
